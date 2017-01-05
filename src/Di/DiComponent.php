c2404fc01eca567a2f439831eaef2747cf29d5f4
Matt Ferris <matt@bueller.ca>
2017-01-04 21:47:41 -0800

    Added DiComponent

---
+++ src/Di/DiComponent.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * bridge-components - Bridge components for mattferris/application
+ * www.bueller.ca/bridge-components
+ *
+ * Di/DiComponent.php
+ * @copyright Copyright (c) 2016 Matt Ferris
+ * @author Matt Ferris <matt@bueller.ca>
+ *
+ * Licensed under BSD 2-clause license
+ * www.bueller.ca/bridge-components/license
+ */
+
+namespace MattFerris\Bridge\Components\Di;
+
+use MattFerris\Application\Component;
+use MattFerris\Di\ContainerInterface;
+
+class DiComponent extends Component
+{
+    /**
+     * @var array Providers
+     */
+    protected $providers = [
+        'Service' => [
+            'consumer' => ContainerInterface::class,
+            'scope' => 'global'
+        ]
+    ];
+}
